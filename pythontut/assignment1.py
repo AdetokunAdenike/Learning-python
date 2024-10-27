a40cf76172a96e736401ada28eb7955dd353f351
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-27 14:35:44 +0100

    Convert miles to kilometers

---
+++ pythontut/assignment1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Recieve miles and convert to kilometers
+# Kilometers = miles * 1.60934
+# Enter miles 5
+# 5 miles equals 8.04 kilometers
+miles = input('Enter miles ')
+miles = int(miles)
+kilometers = miles * 1.60934
+kilometers = float(kilometers)
+print("{} miles equals {} kilometers".format(miles, kilometers))
