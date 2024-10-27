1e1cc6b23f3254b41550f38b4e1628515c158981
AdetokunAdenike <adetokunadenike@gmail.com>
2024-10-27 14:13:40 +0100

    Take user input

---
+++ pythontut/pythontut.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Ask the user to input their name and assign it to a variable named name
+name = input('Insert name ')
+# print out hello folowed by the name they entered
+print("Hello", name)
