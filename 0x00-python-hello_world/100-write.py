e07a8a399407b4776c996f8cd3ba58675d6b8d2c
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-03 14:07:22 +0100

    Update

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+""" A script that prints and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line """
+
+
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+sys.exit(1)
