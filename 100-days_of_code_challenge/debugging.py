5c14f2f9b436518b6ed1ef432c136b7bdd1eee4f
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-19 22:41:47 +0100

    Update

--- 100-days_of_code_challenge/debugging.py
+++ 100-days_of_code_challenge/debugging.py
@@ -1,4 +1,10 @@
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
+print("String Concatenation is done with the \"+\" sign.")
 print('e.g. print("Hello " + "World")')
 print("New lines can be created with a backslash and n.")
+
+# input('Enter your name: ')
+# numofletters = len("Angela")
+# print(numofletters)
+
+print(len(input()))
