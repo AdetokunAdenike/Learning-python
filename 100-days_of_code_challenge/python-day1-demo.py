b107a2e88d4438f4102f3bced512f290665e1dd0
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-19 23:39:25 +0100

    Add: Band name generator

---
+++ 100-days_of_code_challenge/python-day1-demo.py
@@ -0,0 +1,7 @@
+print("Welcome to band name generator.")
+
+name = input('What is your name?\n')
+city = input('What city did you grow up in?\n')
+pet_name = input('What is your pet\'s name?\n')
+
+print("Hello " + name + "! your band name is " + city + pet_name)
