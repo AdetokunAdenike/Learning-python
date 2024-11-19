9fdeabb22e2487e89b2fb66ed2e5b512c6f2ca70
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-19 23:41:04 +0100

    Update

--- 100-days_of_code_challenge/python-day1-demo.py
+++ 100-days_of_code_challenge/python-day1-demo.py
@@ -4,4 +4,4 @@ name = input('What is your name?\n')
 city = input('What city did you grow up in?\n')
 pet_name = input('What is your pet\'s name?\n')
 
-print("Hello " + name + "! your band name is " + city + pet_name)
+print("Hello " + name + "! your band name could be " + city +" "+ pet_name)
