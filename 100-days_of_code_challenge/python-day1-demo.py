a899a13218bf7802c3354724e9d467d9dfab2f0b
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-27 11:03:42 +0100

    Update

--- 100-days_of_code_challenge/python-day1-demo.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to username generator.")
-
-first_name = input('What is your first name?\n')
-last_name = input('What is your last name?\n')
-city = input('What city did you grow up in?\n')
-
-
-print("Hello " + first_name + "! your username could be " + first_name + "_" + last_name + " " + "of" + " " + city)
