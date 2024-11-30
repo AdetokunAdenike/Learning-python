807a4ec9067f792a526f37d2913cc3b4177a3eec
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-30 16:02:29 +0100

    Update

--- 100-days_of_code_challenge/day-4/rock_paper-scissors.py
+++ 100-days_of_code_challenge/day-4/rock_paper-scissors.py
@@ -26,26 +26,28 @@ scissors = '''
       (____)
 '''
  
-game = [rock, paper, scissors]
+game_images = [rock, paper, scissors]
 
-user_choice = int(input('Make a choice, 0 for rock, 1 for paper, or 2 for scissors? '))
-print("You chose: " + game[user_choice])
-computer_choice = random.randint(0, 2)
-print("Computer chose:" + game[computer_choice])
+user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n'))
 
 if user_choice >= 3 or user_choice < 0:
-    print("You must have chosen an invalid input, please try again.")
-elif computer_choice == user_choice:
-    print("It's a draw")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif computer_choice == 0 and user_choice ==2:
-    print("You lose!")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win")
-elif computer_choice == 1 and user_choice == 0:
-    print("You you lose")
-elif user_choice == 2 and computer_choice == 1:
-    print("You win")
-elif computer_choice == 2 and user_choice == 1:
-    print("You lose")
+    print("You typed an invalid number, you lose!")
+else:
+    print("You chose: " + game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print("Computer chose: " + game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == computer_choice:
+        print("It's a draw!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif computer_choice == 1 and user_choice == 0:
+        print("You lose!")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win!")
+    elif computer_choice == 2 and user_choice == 1:
+        print("You lose")
