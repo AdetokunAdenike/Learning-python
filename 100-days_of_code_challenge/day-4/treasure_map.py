77b84de46c548bbaf87f084146bd663496426945
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-30 10:39:41 +0100

    Hide the treasure

--- 100-days_of_code_challenge/day-4/treasure_map.py
+++ 100-days_of_code_challenge/day-4/treasure_map.py
@@ -1,13 +1,32 @@
+# print('''
+#                            ___________
+#                 ___________)%{}%%%%%%/
+#                /{}%%%%%%%%/%%/%%%%%%/
+#               /%%\% _---_/ \/%%%%%%/
+#              /%%%%\/    /()|%%%%%%/
+#             /%%%%%|()  /+ /%%%%%%/
+#            /%%%%%%%\ +/HH%%\%%%%/
+#           /%%%%%%/%HH/_/%%%\%%%/
+#          /%%%%%%/%%\/%%%%%%{}%/
+#         /%%%%%{}%%%/
+#        /
+#       /
+#      /
+#     /
+#    /
+# ''')
+
 line1 = [" ", " ", " "]
 line2 = [" ", " ", " "]
 line3 = [" ", " ", " "]
 
 map = [line1, line2, line3]
-position = input("Where do you want to put the treasure? ")
+position = input('What position do you want to place your treasure? ')
 letter = position[0].lower()
 abc = ["a", "b", "c"]
 letter_index = abc.index(letter)
 number_index = int(position[1]) - 1
 
-map[letter_index][number_index] = "x"
+map[number_index][letter_index] = "X"
+
 print(f"{line1}\n{line2}\n{line3}")
