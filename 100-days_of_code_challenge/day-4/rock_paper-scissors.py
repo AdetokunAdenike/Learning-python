da6a10c8493023106b1a9dbddf964474eb324829
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-30 13:47:55 +0100

    Update

--- 100-days_of_code_challenge/day-4/rock_paper-scissors.py
+++ 100-days_of_code_challenge/day-4/rock_paper-scissors.py
@@ -34,14 +34,18 @@ computer_choice = random.randint(0, 2)
 print("Computer chose:" + game[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
-    print("You must have chose an invalid input, please try again.")
+    print("You must have chosen an invalid input, please try again.")
+elif computer_choice == user_choice:
+    print("It's a draw")
 elif user_choice == 0 and computer_choice == 2:
     print("You win!")
 elif computer_choice == 0 and user_choice ==2:
     print("You lose!")
-elif computer_choice == user_choice:
-    print("It's a draw")
-elif computer_choice > user_choice:
-    print("You lose")
-elif user_choice > computer_choice:
+elif user_choice == 1 and computer_choice == 0:
     print("You win")
+elif computer_choice == 1 and user_choice == 0:
+    print("You you lose")
+elif user_choice == 2 and computer_choice == 1:
+    print("You win")
+elif computer_choice == 2 and user_choice == 1:
+    print("You lose")
